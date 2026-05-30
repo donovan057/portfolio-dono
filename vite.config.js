c8f15037a1a719cb01caafb4eff89b246cdd7bcd
donovan057 <donovanrinquebach25@icloud.com>
2026-05-30 15:54:43 +0200

    Ajout de la page mentions légales dans Vite

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,13 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
     /// Ceci dit à GitHub où trouver tes styles
-    base: '/portfolio-dono/'
+    base: '/portfolio-dono/',
+    build: {
+        rollupOptions: {
+            input: {
+                main: 'index.html',
+                legal: 'mentions-legales.html' // C'est ici qu'on dit à Vite de ne pas oublier cette page !
+            }
+        }
+    }
 })
