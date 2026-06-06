f6b12f4d8ec3ff4a9d846112cec721e2fce7924d
Donovan Rinquebach <donovanrinquebach25@icloud.com>
2026-06-06 16:15:03 +0200

    Build: Configuration de l'input Rollup pour inclure ticket-bot.html

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-    /// Ceci dit à GitHub où trouver tes styles
     base: '/portfolio-dono/',
     build: {
         rollupOptions: {
             input: {
-                main: 'index.html',
-                legal: 'mentions-legales.html' // C'est ici qu'on dit à Vite de ne pas oublier cette page !
+                main: resolveConfig(__dirname, 'index.html'),
+                ticketBot: (__dirname, 'ticket-bot.html'),
+                legal: (__dirname, 'mentions-legales.html')
             }
         }
     }
