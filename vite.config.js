fa8f946e199c09e06953bb358dafa2ca033f7601
Donovan Rinquebach <donovanrinquebach25@icloud.com>
2026-06-06 16:22:11 +0200

    Build: Utilisation des imports natifs node:path et émulation correcte de __dirname

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,8 @@
 import { defineConfig } from 'vite'
+import { fileURLToPath } from 'node:url'
+import path from 'node:path'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
     base: '/portfolio-dono/',
